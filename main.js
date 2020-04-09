81d78df3a8663a9d4c7b51f3fe32b6ccec13ab63
PriyaS1501 <sawantpriyanka323@gmail.com>
2020-04-09 22:53:44 +0530

    break-continue

---
+++ main.js
@@ -0,0 +1,50 @@
+//array declaration
+let arrayObj = [1,2,3,4,5,6,7,8,9,10];
+
+// for each with break statement
+
+for (let value of arrayObj)
+{
+    if (value === 5){
+        break;
+    }
+    console.log(value);
+    //console.log(value); // 1 2 3 4 5 6 7 8 9 10
+}
+
+// assignment : print even and odd number from list of 20 numbers
+
+// even from array
+
+let arrayList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+
+for (let number1 of arrayList)
+{
+    if (number1 % 2 == 0){
+        console.log (number1); // 2,4,6,8,10,12,14,16,18,20
+    }
+  
+}
+
+// odd from array
+
+for (let number2 of arrayList)
+{
+    if(number2 % 2 != 0){
+        console.log(number2); // 1,3,5,7,9,11,13,15,17,19
+    }
+}
+
+
+// continue statement
+
+for (let connumbers of arrayObj){ 
+    if (connumbers === 5){
+        continue;
+    }
+    console.log(connumbers); // 1 2 3 4 6 7 8 9 10
+}
+
+
+
