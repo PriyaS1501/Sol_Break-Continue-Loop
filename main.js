fc8512e765f226e6aa327548dff402a82d3dce18
PriyaS1501 <62501930+PriyaS1501@users.noreply.github.com>
2020-05-03 21:00:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
 // assignment : print even and odd number from list of 20 numbers
 
-// even from array
+
 
 let arrayList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
+// even from array
 
 for (let number1 of arrayList)
 {
